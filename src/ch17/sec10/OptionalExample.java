2a4fcb2d03838610980caee29dbc4668c3d1e148
Jjini <s_nearndear@naver.com>
2024-07-04 17:52:42 +0900

    0705 다섯번째커밋

---
+++ src/ch17/sec10/OptionalExample.java
@@ -0,0 +1,41 @@
+package ch17.sec10;
+
+import java.util.*;
+
+public class OptionalExample {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+////        리스트가 비어있을 경우 java.util.NoSuchElementException 예외 발생
+//        double avg = list.stream()
+//                .mapToInt(Integer::intValue)
+//                .average()
+//                .getAsDouble();
+
+//        방법1
+        OptionalDouble optional = list.stream()
+                .mapToInt(Integer::intValue)
+                .average();
+        if (optional.isPresent()) {
+            System.out.println("방법1_평균: " + optional.getAsDouble());
+        } else {
+            System.out.println("방법1_평균: 0.0");
+        }
+
+//        방법2
+        double avg = list.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+//                orElse: 해당 값이 존재하지 않는 경우 기본값을 정해준다
+                .orElse(0.0);
+        System.out.println("방법2_평균: " + avg);
+
+//        방법3
+        list.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+//                ifPresent: 만약 해당 값이 존재한다면 해당 기능을 실행
+//                현재는 존재하지 않으므로 아무것도 출력되지 않음
+                .ifPresent(a -> System.out.println("방법3_평균: "+ a));
+    }
+}
---
+++ src/ch18/sec02/exam01/WriteExample.java
@@ -0,0 +1,28 @@
+package ch18.sec02.exam01;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteExample {
+    public static void main(String[] args) {
+        try{
+            OutputStream os = new FileOutputStream("C:/temp/test1.db");
+
+            byte a = 10;
+            byte b = 20;
+            byte c = 30;
+
+            os.write(a);
+            os.write(b);
+            os.write(c);
+
+//            flush: 버퍼를 비워준다
+            os.flush();
+//            close: 사용한 스트림을 닫아준다
+            os.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
